1d24873efb8354d39d9c715dea5eaae2e1d7ddb5
lindsey <lindsey.m.baron@gmail.com>
2020-01-01 12:22:23 -0800

    add download dir to config

--- src/util/driver.py
+++ src/util/driver.py
@@ -1,9 +1,9 @@
 import os
-import yaml
 
+import yaml
 from selenium import webdriver
 
-from util.constants import DATA_DIR, CONFIG_PATH, BIN_DIR
+from util.constants import CONFIG_PATH, BIN_DIR
 
 
 def parse_config():
@@ -32,12 +32,42 @@ def get_binary_path():
     return path
 
 
+def get_download_dir():
+    config = parse_config()
+    download_dir_path = config.get('download_dir', '')
+
+    if download_dir_path:
+        print('Attempting to use download directory {}...'.format(download_dir_path))
+        download_dir = os.path.abspath(download_dir_path)
+        # if download_dir is valid, use that
+        if os.path.exists(download_dir):
+            print('Found {}.'.format(download_dir_path))
+            return download_dir_path
+        else:
+            # if download_dir doesn't exist, try to create it
+            print('Could not find {}. Attempting to create it...'.format(download_dir_path))
+            try:
+                os.mkdir(download_dir)
+                print('Created {}.'.format(download_dir_path))
+                return download_dir_path
+            except FileExistsError as e:
+                print(e)
+    else:
+        print('No download directory specified. Using default Chrome download directory (probably ~/Downloads).')
+        return
+
+
 def get_driver(request_id):
     options = webdriver.ChromeOptions()
-    download_dir = os.path.abspath(os.path.join(DATA_DIR, request_id))
-    print('Downloading files to {}.'.format(download_dir))
-    options.add_argument("download.default_directory={}".format(download_dir))
 
+    # set download directory
+    download_dir = get_download_dir()
+    # if download_dir isn't set in config, use default
+    if download_dir:
+        print('Downloading files to {}.'.format(download_dir))
+        options.add_argument("download.default_directory={}".format(download_dir))
+
+    # set binary path
     binary_path = get_binary_path()
 
     return webdriver.Chrome(binary_path, options=options)
