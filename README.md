e1027eb5182d57ef8e20587546164ae6dc56dffd
lindsey <lindsey.m.baron@gmail.com>
2020-02-11 12:20:13 -0800

    update README and add docstrings

--- README.md
+++ README.md
@@ -30,12 +30,19 @@ A hacky bit of python and selenium to navigate the atrociously designed NextRequ
 ### Single Request
 To download all the files for a single records request, use the request ID from the url.
    ```shell script
-   python src/nr.py batch [request-id]
+   python src/nr.py req [request-id]
    ```
 
 ### All Files From 'Documents'
 To download all the files from https://lacity.nextrequest.com/documents:
    ```shell script
    python src/nr.py alldocs
+   ```
+It'll take a long time. As of the time of writing this, there are 4489 of them.
+
+### All Requests
+To fetch all the requests from the 'All Requests' section:
+   ```shell script
+   python src/nr.py allreqs
    ```
 It'll take a long time. As of the time of writing this, there are 4489 of them.
--- src/nr.py
+++ src/nr.py
@@ -12,6 +12,13 @@ class NextRequest(object):
 
     @staticmethod
     def req(req, user=None, pw=None):
+        """Get all documents from a single request.
+
+        Args:
+            req (str): The unique ID of the request.
+            user (str): The email address for the user account. Can also be set in secret.yaml.
+            pw (str): The password for the user account. Can also be set in secret.yaml.
+        """
         user = load_user(email=user, pw=pw)
 
         loop = asyncio.get_event_loop()
@@ -19,6 +26,13 @@ class NextRequest(object):
 
     @staticmethod
     def alldocs(user=None, pw=None):
+        """Download all the documents from the 'All Documents' section.
+            This includes all public documents and any documents visible to the logged in user if logged in.
+
+        Args:
+            user (str): The email address for the user account. Can also be set in secret.yaml.
+            pw (str): The password for the user account. Can also be set in secret.yaml.
+        """
         rsession = requests.Session()
         login_session(rsession, load_user(email=user, pw=pw))
 
@@ -27,6 +41,13 @@ class NextRequest(object):
 
     @staticmethod
     def allreqs(user=None, pw=None):
+        """Fetch all the requests from the 'All Requests' section.
+            This includes all public requests and any requests visible to the logged in user if logged in.
+
+        Args:
+            user (str): The email address for the user account. Can also be set in secret.yaml.
+            pw (str): The password for the user account. Can also be set in secret.yaml.
+        """
         rsession = requests.Session()
         login_session(rsession, load_user(email=user, pw=pw))
 
